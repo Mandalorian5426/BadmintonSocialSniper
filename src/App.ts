762e489fb4a4df6383dc264c968e2eeb13a3e703
Liam Wilburn <liam@hive9.systems>
2021-06-25 08:34:17 +0800

    Send message when name has been added

--- src/App.ts
+++ src/App.ts
@@ -2,10 +2,14 @@ import * as venom from "venom-bot";
 import { Message, Whatsapp } from "venom-bot";
 
 const WEEKDAY_SOCIAL_CHAT_ID = "61411527010-1612698201@g.us";
+
 const PBA_REDCLIFFE_ID = "61411527010@c.us";
 const MIKE_TAN_ID = "61423965711@c.us";
-const MESSAGE_REGEX =
+
+const THURSDAY_MESSAGE_REGEX =
   /baddy social @ pba redcliffe.*thursday.*6pm-8pm.*(?<!list[ \n]*)1\.[ \n]*[^a-z0-9 ].*/is;
+const TUESDAY_MESSAGE_REGEX =
+  /baddy social @ pba redcliffe.*tuesday.*6pm-8pm.*(?<!list[ \n]*)1\.[ \n]*[^a-z0-9 ].*/is;
 
 venom
   .create("Liam Wilburn")
@@ -14,28 +18,33 @@ venom
     console.log(error);
   });
 
-function start(client: Whatsapp) {
+const start = (client: Whatsapp) => {
   client.onMessage((message: Message) => {
-    if (
-      (message.chatId as any)["_serialized"] === WEEKDAY_SOCIAL_CHAT_ID &&
-      ((message.sender.id as any)["_serialized"] === PBA_REDCLIFFE_ID ||
-        (message.sender.id as any)["_serialized"] === MIKE_TAN_ID) &&
-      MESSAGE_REGEX.test(message.body)
-    ) {
-      let nameEntered = false;
-      console.info(
-        message.body
-          .split("\n")
-          .map((text) => {
-            if (text.startsWith("1.") && !nameEntered) {
-              nameEntered = true;
-              return text + "Liam W";
-            } else {
-              return text;
-            }
-          })
-          .join("\n")
-      );
+    if (isInitialMessage(message)) {
+      const newMessage = addNameToInitialMessage(message);
+      client.sendText(WEEKDAY_SOCIAL_CHAT_ID, newMessage);
     }
   });
-}
+};
+
+const isInitialMessage = (message: Message) =>
+  (message.chatId as any)["_serialized"] === WEEKDAY_SOCIAL_CHAT_ID &&
+  ((message.sender.id as any)["_serialized"] === PBA_REDCLIFFE_ID ||
+    (message.sender.id as any)["_serialized"] === MIKE_TAN_ID) &&
+  (THURSDAY_MESSAGE_REGEX.test(message.body) ||
+    TUESDAY_MESSAGE_REGEX.test(message.body));
+
+const addNameToInitialMessage = (message: Message) => {
+  let nameEntered = false;
+  return message.body
+    .split("\n")
+    .map((text) => {
+      if (text.startsWith("1.") && !nameEntered) {
+        nameEntered = true;
+        return text + "Liam W";
+      } else {
+        return text;
+      }
+    })
+    .join("\n");
+};
